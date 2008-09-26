f3189bcfe621c90147bc157467759782f525586e
kai chen <chenk85@gmail.com>
2008-09-26 16:34:30 +0800

    refacter some code

--- lib/hbase/model.rb
+++ lib/hbase/model.rb
@@ -2,8 +2,6 @@ module HBase
   module Model
     class Record
       def initialize (params)
-        return if params.nil?
-
         params.each do |key, value|
           name = key.to_s
           instance_variable_set("@#{name}", value) if respond_to?(name)
