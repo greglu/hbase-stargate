0b361b8d8a851f18f6bf4397f906295b0ce8778f
kai chen <chenk85@gmail.com>
2008-10-15 14:26:51 +0800

    add spec hbase/model stub

---
+++ spec/hbase/model/column_spec.rb
@@ -0,0 +1,7 @@
+require File.join(File.dirname(__FILE__), "..", "..", "spec_helper")
+
+describe HBase::Model::Column do
+
+  it "should be ..."
+
+end
---
+++ spec/hbase/model/region_descriptor_spec.rb
@@ -0,0 +1,7 @@
+require File.join(File.dirname(__FILE__), "..", "..", "spec_helper")
+
+describe HBase::Model::RegionDescriptor do
+
+  it "should be ..."
+
+end
