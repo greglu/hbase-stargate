47aad9c1c1ff681ed3d633e48c289d9b3dd6bcfb
Ye Dingding <sishen@sishen-mac.local>
2008-10-16 02:32:58 +0800

    nil exception

--- lib/hbase/response/scanner_response.rb
+++ lib/hbase/response/scanner_response.rb
@@ -9,7 +9,7 @@ module HBase
           columns = []
           row.elements.each("column") do |col|
             name = col.elements["name"].text.strip.unpack("m").first
-            value = col.elements["value"].text.strip.unpack("m").first
+            value = col.elements["value"].text.strip.unpack("m").first rescue nil
             timestamp = col.elements["timestamp"].text.strip.to_i
             columns << Model::Column.new(:name => name,
                                          :value => value,
